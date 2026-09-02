3d68ad1a3b9ac90005648f047097522c56aa96d1
RegroupForces <161395559+RegroupForces@users.noreply.github.com>
2026-09-02 15:58:15 +1000

    Published version 1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Code for Tunnel Ave Control Subsystem
 # Created by: Jianqiu (Jacky) Dong
 # Date Created: 26/08/2026
-# Last Modified: 01/09/2026
+# Last Modified: 02/09/2026
+# Version 1.0
 
 
 
@@ -55,7 +56,7 @@ def callback(data: list):
 
 
 
-def pushButtonCheck(board: pymata4.Pymata4):
+def pushButtonCheck():
     """
     Helper function that check for a 'switch on' signal.
     Fetches signal from a predefined list (callbackStorage).
@@ -63,8 +64,7 @@ def pushButtonCheck(board: pymata4.Pymata4):
     Clears callbackStorage upon returning
 
     Args:
-        board: pymata4.Pymata4
-        The board initialised by main()
+        None
     
     Returns:
         action: int | None
@@ -130,7 +130,26 @@ def main():
     #[red, yellow, green]
     TL4 = [False, False, False]
     TL5 = [False, False, False]
+    cycleCounter = 0 #counter for timer
+    cycleState = 0 #current state of the subsystem. 
+    # If 0, TL4 is green. 
+    # Elseif 1, TL4 is yellow in transition cycle.
+    # Elseif 2, TL5 is green.
+    # ELseif 3, TL5 is yellow in transition cycle.
     
+    # Initial setup for system:
+    # TL4 is green, all else red
+    board.digital_write(trafficLightsPins[0], 0)
+    board.digital_write(trafficLightsPins[1], 0)
+    board.digital_write(trafficLightsPins[2], 1)
+    board.digital_write(trafficLightsPins[3], 1)
+    board.digital_write(trafficLightsPins[4], 0)
+    board.digital_write(trafficLightsPins[5], 0)
+    board.digital_write(pedestrianLightsPins[0], 1)
+    board.digital_write(pedestrianLightsPins[1], 0)
+    TL4 = [False, False, True]
+    TL5 = [True, False, False]
+
     try:
         while True:
             # the main cycle operates every 1 second. This is subject to change for integration with ultrasonic sensor.
@@ -141,12 +160,22 @@ def main():
                 time.sleep(2)
                 if TL5[0]:
                     # TL5 is red, turn TL4 to yellow
-                    board.digital_write(trafficLightsPins[2], 0)
+                    board.digital_write(trafficLightsPins[0], 0)
                     board.digital_write(trafficLightsPins[1], 1)
+                    board.digital_write(trafficLightsPins[2], 0)
                     time.sleep(3)
                     #turn TL4 to red
-                    board.digital_write(trafficLightsPins[1], 0)
                     board.digital_write(trafficLightsPins[0], 1)
+                    board.digital_write(trafficLightsPins[1], 0)
+                else:
+                    # TL5 is not red, turn TL5 to yellow
+                    board.digital_write(trafficLightsPins[3], 0)
+                    board.digital_write(trafficLightsPins[4], 1)
+                    board.digital_write(trafficLightsPins[5], 0)
+                    time.sleep(3)
+                    #turn TL5 to red
+                    board.digital_write(trafficLightsPins[3], 1)
+                    board.digital_write(trafficLightsPins[4], 0)
 
                 #set PL1/2 to green
                 board.digital_write(pedestrianLightsPins[0], 0)
@@ -160,17 +189,80 @@ def main():
                     board.digital_write(pedestrianLightsPins[0], 0)
                     time.sleep(0.25)
                 board.digital_write(pedestrianLightsPins[0], 1)
-                board.digital_write(trafficLightsPins[2], 0)
+                # PLs is now red, change TL4 to green and start new main cycle from here
+                board.digital_write(trafficLightsPins[0], 0)
+                board.digital_write(trafficLightsPins[2], 1)
+                cycleCounter = 0
+                cycleState = 0
+                continue
+
+            # This is the main cycle
+            if cycleCounter >= 20 and cycleState == 0:
+                # If TL4 has been green for 20 sec
+                # Turn TL4 to yellow
+                board.digital_write(trafficLightsPins[0], 0)
                 board.digital_write(trafficLightsPins[1], 1)
+                board.digital_write(trafficLightsPins[2], 0)
+                TL4 = [False, True, False]
+                cycleCounter = 0
+                cycleState = 1
+                continue
+
+            elif cycleCounter >= 3 and cycleState == 1:
+                # If TL4 has passed the 3 second yellow light
+                # Turn TL4 to red and TL5 to green
+                board.digital_write(trafficLightsPins[0], 1)
+                board.digital_write(trafficLightsPins[1], 0)
+                board.digital_write(trafficLightsPins[2], 0)
+                board.digital_write(trafficLightsPins[3], 0)
+                board.digital_write(trafficLightsPins[4], 0)
+                board.digital_write(trafficLightsPins[5], 1)
+                TL4 = [True, False, False]
+                TL5 = [False, False, True]
+                cycleCounter = 0
+                cycleState = 2
+                continue
 
 
-            #Logic for main cycle
+            elif cycleCounter >= 10 and cycleState == 2:
+                # If TL5 has been green for 10 sec
+                # Turn TL5 to yellow
+                board.digital_write(trafficLightsPins[3], 0)
+                board.digital_write(trafficLightsPins[4], 1)
+                board.digital_write(trafficLightsPins[5], 0)
+                TL5 = [False, True, False]
+                cycleCounter = 0
+                cycleState = 3
+                continue
+
+            elif cycleCounter >= 3 and cycleState == 3:
+                # If TL5 has passed the 3 second yellow light
+                # Turn TL5 to red and TL4 to green
+                board.digital_write(trafficLightsPins[0], 0)
+                board.digital_write(trafficLightsPins[1], 0)
+                board.digital_write(trafficLightsPins[2], 1)
+                board.digital_write(trafficLightsPins[3], 1)
+                board.digital_write(trafficLightsPins[4], 0)
+                board.digital_write(trafficLightsPins[5], 0)
+                TL4 = [False, False, True]
+                TL5 = [True, False, False]
+                cycleCounter = 0
+                cycleState = 0
+                continue
+
+            else:
+                # Nothing important happens in cycle, increment counter
+                cycleCounter += 1
+
+            #Universal sleep for all cycles
+            time.sleep(1)
+
+
 
 
                 
     except KeyboardInterrupt:
         terminate(board, trafficLightsPins + pedestrianLightsPins)
-
     pass
 
 
